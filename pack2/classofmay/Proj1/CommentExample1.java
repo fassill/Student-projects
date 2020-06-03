ace213a5768ce80fab81385472c5849bcf8d3faa
fassill <65687647+fassill@users.noreply.github.com>
2020-06-02 19:49:54 -0700

    basics with variables

---
+++ pack2/classofmay/Proj1/CommentExample1.java
@@ -0,0 +1,11 @@
+
+public class CommentExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10; //example of the letter i being used as a variable
+		System.out.println(i);
+	
+	}
+
+}
