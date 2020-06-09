9f3b79b9c510ba34251794c95d46db544f07b617
fassill <65687647+fassill@users.noreply.github.com>
2020-06-09 15:23:18 -0700

    array 2

---
+++ proj3/ArrayClass1.java
@@ -0,0 +1,26 @@
+package proj3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] nameOfCommon = new String[60];  //0-59
+		
+		nameOfCommon[0] = "Fassil";
+		nameOfCommon[1] = "Alemayhu";
+		nameOfCommon[2] = "Hirut";
+		nameOfCommon[3] = "Tena";
+		nameOfCommon[4] = "Zemed";
+		
+		System.out.println(nameOfCommon[0]);
+		System.out.println(nameOfCommon[1]);
+		System.out.println(nameOfCommon[2]);
+		System.out.println(nameOfCommon[3]);
+		System.out.println(nameOfCommon[4]);
+	}
+
+}
